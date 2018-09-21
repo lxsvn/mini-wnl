da6754c89e98794a877b2fdbb8598816bc128066
q763278661 <763278661@qq.com>
2018-09-21 14:14:27 +0800

    cf

--- pages/festival/festival.js
+++ pages/festival/festival.js
@@ -1,47 +1,79 @@
 // pages/festival/festival.js
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    items: [
+      {
+        "name": "中秋节",
+        "day": "9月24日",
+        "oldday":'3'
+      },
+      {
+        "name": "国庆节",
+        "day": "10月01日",
+        "oldday": '10'
+      },
+      {
+        "name": "元旦节",
+        "day": "2019年01月01日",
+        "oldday": '102'
+      },
+      {
+        "name": "春节",
+        "day": "2019年02月05日",
+        "oldday": '137'
+      },
+      {
+        "name": "清明",
+        "day": "2019年04月05日",
+        "oldday": '196'
+      },
+      {
+        "name": "劳动节",
+        "day": "2019年05月01日",
+        "oldday": '222'
+      },
+      {
+        "name": "端午节",
+        "day": "2019年06月07日",
+        "oldday": '259'
+      },
+      {
+        "name": "中秋节",
+        "day": "2019年09月13日",
+        "oldday": '357'
+      },
+    ],
+    index:'',
+    
   },
   onChangeShowState: function () {
-
     var that = this;
-
-    
     that.setData({
       ifture: (!that.data.ifture)
     })
-
   },
   onChangeShowtwo: function () {
-
     var that = this;
-
-
     that.setData({
       iftwo: (!that.data.iftwo)
     })
-
   },
   onChangeShowhot: function () {
-
     var that = this;
-
-
     that.setData({
       ifhot: (!that.data.ifhot)
     })
-
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+   
   },
 
   /**
--- pages/index/index.js
+++ pages/index/index.js
@@ -11,23 +11,31 @@ Page({
    * 页面的初始数据
    */
   data: {
-    weather1:[],
+    array: ['白羊座', '金牛座', '双子座', '巨蟹座', "狮子座", '处女座', '天蝎座', '射手座', '摩羯座', "水瓶座", "双鱼座"],
+    weather1: [],
     weather2: [],
     weather3: [],
-    data:[],
-    suit:[],
-    taboo:[],
-    weather:[],
-    high0:[],
-    low0:[],
+    data: [],
+    suit: [],
+    taboo: [],
+    weather: [],
+    high0: [],
+    low0: [],
     high1: [],
     low1: [],
     high2: [],
     low2: [],
     date: '2018-09-20',
-    year:[],   
-    day:[],
-    dacity:"北京"
+    year: [],
+    day: [],
+    dacity: "北京",
+    index: 3,
+    currentDate: "2017年05月03日",
+    dayList: '',
+    currentDayList: '',
+    currentObj: '',
+    currentDay: '',
+    aaa: '2018年9月21 '
   },
   onChangeShowState: function () {
 
@@ -39,15 +47,19 @@ Page({
 
     })
     that.setData({
-      ifture:(!that.data.ifture)
+      ifture: (!that.data.ifture)
     })
 
   },
   changeDate(e) {
-    
-    this.setData({ 
-      date: e.detail.value,
-       });
+
+    // 
+    let aa = e.detail.value.replace(/^(.{4})(.{1})(.*)$/, '$1年$3');
+    let aaa = aa.replace(/^(.{7})(.{1})(.*)$/, '$1月$3');
+    console.log(aaa)
+    this.setData({
+      aaa: aaa,
+    });
   },
   changecity(e) {
     console.log(e.detail.value);
@@ -59,7 +71,6 @@ Page({
 
       url: 'https://www.sojson.com/open/api/weather/json.shtml?city=' + dacity,
       success: (msg) => {
-        console.log(msg)
         let high0 = msg.data.data.forecast[0].high.substring(2, 5);
         let low0 = msg.data.data.forecast[0].low.substring(2, 5);
         let high1 = msg.data.data.forecast[1].high.substring(2, 5);
@@ -90,30 +101,112 @@ Page({
       day: this.data.days[val[2]]
     })
   },
-  
+  bindPickerChange(e) {
 
+    // this.setData({
+    //   date: e.detail.value,
+    // });
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      index: e.detail.value
+    })
+  },
+  doDay: function (e) {
+    var that = this
+    var currentObj = that.data.currentObj
+    var Y = currentObj.getFullYear();
+    var m = currentObj.getMonth() + 1;
+    var d = currentObj.getDate();
+    var str = ''
+    if (e.currentTarget.dataset.key == 'left') {
+      m -= 1
+      if (m <= 0) {
+        str = (Y - 1) + '/' + 12 + '/' + d
+      } else {
+        str = Y + '/' + m + '/' + d
+      }
+    } else {
+      m += 1
+      if (m <= 12) {
+        str = Y + '/' + m + '/' + d
+      } else {
+        str = (Y + 1) + '/' + 1 + '/' + d
+      }
+    }
+    currentObj = new Date(str)
+    this.setData({
+      currentDate: currentObj.getFullYear() + '年' + (currentObj.getMonth() + 1) + '月' + currentObj.getDate() + '日',
+      currentObj: currentObj
+    })
+    this.setSchedule(currentObj);
+  },
+
+  getCurrentDayString: function () {
+    var objDate = this.data.currentObj
+    if (objDate != '') {
+      return objDate
+    } else {
+      var c_obj = new Date()
+      var a = c_obj.getFullYear() + '/' + (c_obj.getMonth() + 1) + '/' + c_obj.getDate()
+      return new Date(a)
+    }
+  },
+  setSchedule: function (currentObj) {
+    var that = this
+    var m = currentObj.getMonth() + 1
+    var Y = currentObj.getFullYear()
+    var d = currentObj.getDate();
+    var dayString = Y + '/' + m + '/' + currentObj.getDate()
+    var currentDayNum = new Date(Y, m, 0).getDate()
+    var currentDayWeek = currentObj.getUTCDay() + 1
+    var result = currentDayWeek - (d % 7 - 1);
+    var firstKey = result <= 0 ? 7 + result : result;
+    var currentDayList = []
+    var f = 0
+    for (var i = 0; i < 42; i++) {
+      let data = []
+      if (i < firstKey - 1) {
+        currentDayList[i] = ''
+      } else {
+        if (f < currentDayNum) {
+          currentDayList[i] = f + 1
+          f = currentDayList[i]
+        } else if (f >= currentDayNum) {
+          currentDayList[i] = ''
+        }
+      }
+    }
+    that.setData({
+      currentDayList: currentDayList
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
-    wx:wx.request({
-      
+    var time = dateTimePicker.formatTime(new Date());
+
+    let day = time.substring(8);
+    this.setData({
+      day: day
+    })
+    wx: wx.request({
+
       url: 'https://www.sojson.com/open/api/weather/json.shtml?city=小店区',
-      success:(msg)=>{
+      success: (msg) => {
         console.log(msg)
-        let high0 = msg.data.data.forecast[0].high.substring(2,5);
+        let high0 = msg.data.data.forecast[0].high.substring(2, 5);
         let low0 = msg.data.data.forecast[0].low.substring(2, 5);
         let high1 = msg.data.data.forecast[1].high.substring(2, 5);
         let low1 = msg.data.data.forecast[1].low.substring(2, 5);
         let high2 = msg.data.data.forecast[2].high.substring(2, 5);
         let low2 = msg.data.data.forecast[2].low.substring(2, 5);
-        
+
         this.setData({
           weather1: msg.data.data.forecast[0],
           weather2: msg.data.data.forecast[1],
           weather3: msg.data.data.forecast[2],
-          weather:msg.data,
+          weather: msg.data,
           high0: high0,
           low0: low0,
           high1: high1,
@@ -123,7 +216,13 @@ Page({
         })
       }
     })
-    
+    var currentObj = this.getCurrentDayString()
+    this.setData({
+      currentDate: currentObj.getFullYear() + '年' + (currentObj.getMonth() + 1) + '月' + currentObj.getDate() + '日',
+      currentDay: currentObj.getDate(),
+      currentObj: currentObj
+    })
+    this.setSchedule(currentObj)
   },
 
   /**
@@ -131,7 +230,7 @@ Page({
    */
   onReady: function () {
     var time = dateTimePicker.formatTime(new Date());
-   
+
     // wx: wx.request({
     //   url: 'http://v.juhe.cn/laohuangli/d?key=48d0e29d484984c057193f9a85b05be3&date=' + time,
     //   success: (msg) => {
@@ -149,41 +248,40 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-   
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
-  },
 
+  },
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    
+
   }
 })
